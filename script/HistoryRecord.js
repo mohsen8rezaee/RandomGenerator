dc42a317ed0748ff481d0fd94da78ee7aa2d4871
mohsen8 <8mohsen8rezaee8@gmail.com>
2025-12-26 21:14:35 +0330

    add models

---
+++ script/HistoryRecord.js
@@ -0,0 +1,9 @@
+class HistoryRecord {
+    constructor (countOfRecord,dateGenerate,records) {
+        this.countOfRecord=countOfRecord
+        this.dateGenerate=dateGenerate
+        this.records=records
+    }
+
+
+}
---
+++ script/Record.js
@@ -0,0 +1,9 @@
+class Record {
+    constructor (name, family , age , email) {
+        this.name = name
+        this.family=family
+        this.age=age
+        this.email=email
+    }
+    
+}
